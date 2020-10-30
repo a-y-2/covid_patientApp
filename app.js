5f614103b3e69d9b09c4dd198f54dac553dbe283
socopoko <59167401+socopoko@users.noreply.github.com>
2020-10-30 16:48:58 +0530

    updated header and footer, added materialize cdn

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
-var express                 = require("express"),
-    passport                = require("passport"),
-    bodyParser              = require("body-parser"),
-    User                    = require("./models/user"),
-    LocalStrategy           = require("passport-local"),
-    passportLocalMongoose   = require("passport-local-mongoose") 
+var express = require("express"),
+  passport = require("passport"),
+  bodyParser = require("body-parser"),
+  User = require("./models/user"),
+  LocalStrategy = require("passport-local"),
+  passportLocalMongoose = require("passport-local-mongoose");
 
-        
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/auth_demo_app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => console.log('Connected to DB!'))
-.catch(error => console.log(error.message));
-
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost:27017/auth_demo_app", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to DB!"))
+  .catch((error) => console.log(error.message));
 
 var app = express();
-app.set('view engine','ejs');
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(require("express-session")({
-    secret:"ayushi",
-    resave:false,
-    saveUninitialized:false
-}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  require("express-session")({
+    secret: "ayushi",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
@@ -32,63 +33,66 @@ passport.deserializeUser(User.deserializeUser());
 //routes
 //-----------------------------
 
-
-
-
-app.get("/",function(req,res){
-    res.render("home");
+app.get("/", function (req, res) {
+  res.render("home");
 });
-app.get("/dashboard",isLoggedIn, function(req,res){
-    res.render("dashboard");
+app.get("/dashboard", isLoggedIn, function (req, res) {
+  res.render("dashboard");
 });
 //auth routes
 //show sign up form
-app.get("/register" , function(req,res){
-    res.render("register");});  
+app.get("/register", function (req, res) {
+  res.render("register");
+});
 
 //handling user signup
-app.post("/register" , function(req,res){
-    req.body.username
-    req.body.password
-    User.register(new User({username: req.body.username}),req.body.password,function(err,user){
-        if(err){
-            console.log(err);
-            return res.render('register');
-        }
-        passport.authenticate("local")(req, res, function(){
-            res.redirect("/dashboard");
-        });
-    });
+app.post("/register", function (req, res) {
+  req.body.username;
+  req.body.password;
+  User.register(
+    new User({ username: req.body.username }),
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        return res.render("register");
+      }
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/dashboard");
+      });
+    }
+  );
 });
 
 //login routes
 //render login form
-app.get("/login" , function(req,res){
-    res.render("login");
+app.get("/login", function (req, res) {
+  res.render("login");
 });
 
 //login logic
 //middleware
-app.post("/login" , passport.authenticate("local" , {
-    successRedirect:"/dashboard",
-    failureRedirect:"/login"
-}) ,function(req,res){
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/login",
+  }),
+  function (req, res) {}
+);
 
-});
- 
-app.get("/logout" , function(req,res){
-    req.logout();
-    res.redirect("/");
-    
+app.get("/logout", function (req, res) {
+  req.logout();
+  res.redirect("/");
 });
 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("/login");
 }
 
-app.listen(3000,function(){
-    console.log("serving");
+app.listen(3000, function () {
+  console.log("serving");
 });
